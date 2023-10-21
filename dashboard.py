3f01b387e0f3608d09a50b9ce1554e06ae50a5ef
zeinrivo <110659540+zeinrivo@users.noreply.github.com>
2023-10-21 22:00:28 +0700

    Create dashboard.py

---
+++ dashboard.py
@@ -0,0 +1,127 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+
+sns.set(style='dark')
+
+def get_total_count_by_hour_df(hour_df):
+  hour_count_df =  hour_df.groupby(by="hours").agg({"count_cr": ["sum"]})
+  return hour_count_df
+
+def count_by_day_df(day_df):
+    day_df_count_2011 = day_df.query(str('dteday >= "2011-01-01" and dteday < "2012-12-31"'))
+    return day_df_count_2011
+
+def total_registered_df(day_df):
+   reg_df =  day_df.groupby(by="dteday").agg({
+      "registered": "sum"
+    })
+   reg_df = reg_df.reset_index()
+   reg_df.rename(columns={
+        "registered": "register_sum"
+    }, inplace=True)
+   return reg_df
+
+def sum_order (hour_df):
+    sum_order_items_df = hour_df.groupby("hours").count_cr.sum().sort_values(ascending=False).reset_index()
+    return sum_order_items_df
+
+def macem_season (day_df):
+    season_df = day_df.groupby(by="season").count_cr.sum().reset_index()
+    return season_df
+
+days_df = pd.read_csv("dayc.csv")
+hours_df = pd.read_csv("hourc.csv")
+
+datetime_columns = ["dteday"]
+days_df.sort_values(by="dteday", inplace=True)
+days_df.reset_index(inplace=True)
+
+hours_df.sort_values(by="dteday", inplace=True)
+hours_df.reset_index(inplace=True)
+
+for column in datetime_columns:
+    days_df[column] = pd.to_datetime(days_df[column])
+    hours_df[column] = pd.to_datetime(hours_df[column])
+
+min_date_days = days_df["dteday"].min()
+max_date_days = days_df["dteday"].max()
+
+min_date_hour = hours_df["dteday"].min()
+max_date_hour = hours_df["dteday"].max()
+
+with st.sidebar:
+    start_date, end_date = st.date_input(
+        label='Rentang Waktu',
+        min_value=min_date_days,
+        max_value=max_date_days,
+        value=[min_date_days, max_date_days])
+
+main_df_days = days_df[(days_df["dteday"] >= str(start_date)) &
+                       (days_df["dteday"] <= str(end_date))]
+
+main_df_hour = hours_df[(hours_df["dteday"] >= str(start_date)) &
+                        (hours_df["dteday"] <= str(end_date))]
+
+hour_count_df = get_total_count_by_hour_df(main_df_hour)
+day_df_count_2011 = count_by_day_df(main_df_days)
+reg_df = total_registered_df(main_df_days)
+sum_order_items_df = sum_order(main_df_hour)
+season_df = macem_season(main_df_hour)
+
+st.header('Bike Sharing')
+
+st.subheader('Daily Sharing')
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    total_orders = day_df_count_2011.count_cr.sum()
+    st.metric("Total Sharing Bike", value=total_orders)
+
+with col2:
+    total_sum = reg_df.register_sum.sum()
+    st.metric("Total Registered", value=total_sum)
+
+with col3:
+    total_sum = cas_df.casual_sum.sum()
+    st.metric("Total Casual", value=total_sum)
+
+st.subheader("Pada pukul berapa penyewaan sepeda terbanyak ?")
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35, 15))
+
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.head(5), palette=["#F08080", "#F08080", "#8B0000", "#F08080", "#F08080"], ax=ax[0])
+ax[0].set_ylabel(None)
+ax[0].set_xlabel("Hours (PM)", fontsize=30)
+ax[0].set_title("Jam dengan penyewaan sepeda terbanyak", loc="center", fontsize=30)
+ax[0].tick_params(axis='y', labelsize=35)
+ax[0].tick_params(axis='x', labelsize=30)
+
+sns.barplot(x="hours", y="count_cr", data=sum_order_items_df.sort_values(by="hours", ascending=True).head(5), palette=["#FFC0CB", "#FFC0CB", "#FFC0CB", "#FFC0CB","#C71585"], ax=ax[1])
+ax[1].set_ylabel(None)
+ax[1].set_xlabel("Hours (AM)",  fontsize=30)
+ax[1].set_title("Jam dengan penyewaan sepeda terendah", loc="center", fontsize=30)
+ax[1].invert_xaxis()
+ax[1].yaxis.set_label_position("right")
+ax[1].yaxis.tick_right()
+ax[1].tick_params(axis='y', labelsize=35)
+ax[1].tick_params(axis='x', labelsize=30)
+
+st.pyplot(fig)
+st.subheader("Pada musim apa penyewaan sepeda terbanyak ?")
+
+colors = ["#F08080", "#F08080", "#8B0000", "#F08080"]
+fig, ax = plt.subplots(figsize=(50, 30))
+sns.barplot(
+        y="count_cr",
+        x="season",
+        data=season_df.sort_values(by="season", ascending=False),
+        palette=colors,
+        ax=ax
+    )
+ax.set_title("Plot Perbandingan Antar Musim", loc="center", fontsize=70)
+ax.set_ylabel(None)
+ax.set_xlabel(None)
+ax.tick_params(axis='x', labelsize=35)
+ax.tick_params(axis='y', labelsize=30)
+st.pyplot(fig)
